5fc120454d302a71801b35f36ed23c6037057190
baekbum <baekbum828@naver.com>
2021-08-24 21:03:43 +0900

    웹소켓 처리를 위한 config, controller 및 message 클래스 정의

--- src/main/java/com/example/back/api/controller/websocket/WebSocketController.java
+++ src/main/java/com/example/back/api/controller/websocket/WebSocketController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-//@CrossOrigin(origins = "http://localhost:3000")
 public class WebSocketController {
 
     @MessageMapping("/send-message")
