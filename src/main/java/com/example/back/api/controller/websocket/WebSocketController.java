cf7098cf0c8a0c2d76b875927c8e56cb549696cb
baekbum <baekbum828@naver.com>
2021-09-01 21:27:59 +0900

    주석 수정

--- src/main/java/com/example/back/api/controller/websocket/WebSocketController.java
+++ src/main/java/com/example/back/api/controller/websocket/WebSocketController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WebSocketController {
 
-//    @MessageMapping("/send-message/{roomId}") 특정 룸에 메시지를 보내고 싶을 때때
+//    @MessageMapping("/send-message/{roomId}") 특정 룸에 메시지를 보내고 싶을 때
 //   @SendTo("/topic/message/{roomId}")
     @MessageMapping("/send-message")
     @SendTo("/topic/message")
